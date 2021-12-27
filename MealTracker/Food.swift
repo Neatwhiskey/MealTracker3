e00aaab92d256c4efe28ea6ec92c04ffbf34ac93
neatwhiskey <neatwhiskey24@gmail.com>
2021-12-27 10:40:58 +0100

    Initial Commit

---
+++ MealTracker/Food.swift
@@ -0,0 +1,12 @@
+//
+//  food.swift
+//  MealTracker
+//
+//  Created by Jamaaldeen Opasina on 25/12/2021.
+//
+
+import Foundation
+struct Food{
+    var name: String
+    var description: String
+}
---
+++ MealTracker/meal.swift
@@ -0,0 +1,12 @@
+//
+//  meal.swift
+//  MealTracker
+//
+//  Created by Jamaaldeen Opasina on 25/12/2021.
+//
+
+import Foundation
+struct meal{
+    var name: String
+    var food: [Food]
+}
